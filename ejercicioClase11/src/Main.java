d61d4d17f8347b6b281d94dd76434daa62293996
germanpistan <anibalgermanpistan@gmail.com>
2023-04-14 13:45:29 -0300

    clase 11

--- ejercicioClase11/src/Main.java
+++ ejercicioClase11/src/Main.java
@@ -1,8 +1,8 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -18,30 +18,40 @@ public class Main {
         String mensaje ="";
         String receptor ="";
         LocalDateTime fecha = LocalDateTime.now();
+        String ruta = "C:\\Users\\Carolina\\Desktop\\ARGENTINA PROGRAMA\\ejercicioClase11\\archivoMensajes.csv";
+        String mensajesChat = "Emisor;Receptor;Mensaje;FechaHora";
         switch (opcion) {
             case "1" :{
                 System.out.println("Ingrese su mensaje: ");
                 mensaje =sc.nextLine();
                 System.out.println("Ingrese el destinatario: ");
                 receptor =sc.nextLine();
-                String ruta = "C:\\Users\\Carolina\\Desktop\\ARGENTINA PROGRAMA\\ejercicioClase11\\archivosMensajes.csv";
-                String mensajesChat = "";
+
                 try {
                     mensajesChat = Files.readString(Paths.get(ruta));
                     mensajesChat = mensajesChat + "\n" +
                             nombre + ";" +
                             receptor + ";" +
-                            mensaje + ";" + 
+                            mensaje + ";" +
+                            fecha.toString();
+                    Files.write(Paths.get(ruta), mensajesChat.getBytes());  //metodo write lo pide con getBytes
 
                 } catch (IOException e) {
                     throw new RuntimeException(e);
                 }
-
-
                 break;
             }
             case "2" :{
-                System.out.println("")
+                List <String> lineas;
+                try {
+                    lineas= Files.readAllLines(Paths.get(ruta));
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+                for (int i=1; i<lineas.size(); i++) {
+                    String[] linea = lineas.get(i).split(";");
+                    System.out.println(linea[2]);
+                }
                 break;
             }
 
